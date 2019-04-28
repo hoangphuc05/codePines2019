cd37c5814b70976dd68cabfcb915850d85d63c2d
Phuc H Cai <pcai22@my.whitworth.edu>
2019-04-27 20:33:42 -0700

    8:33pm

---
+++ test4.py
@@ -0,0 +1,103 @@
+#https://stackoverflow.com/questions/960733/python-creating-a-dictionary-of-lists
+
+import requests
+import glob, os
+from collections import defaultdict
+
+def listClass(startLoc, whitworth, listStore):
+    #startLoc = majorList[list(majorList)[firstChoice]]
+    #endLoc = whitworth.find('"sc_courselist"', startLoc+1)
+    endLoc = whitworth.find('"sc_courselist"', startLoc+1) 
+
+    comment = ''
+    commentLoc = whitworth.find('span class="courselistcomment"', startLoc) 
+    #print (whitworth[commentLoc:commentLoc + 4])
+    #while commentLoc != -1:
+    #    comment.append(whitworth[commentLoc + 31:commentLoc + 35])
+    #    commentLoc = whitworth.find('span class="courselistcomment"', commentLoc+1, endLoc)
+
+    #print (comment)
+
+    majorNameLoc = whitworth.find('name="majors_optionstext">Requirements for a', startLoc, endLoc)
+    
+    #print (whitworth[majorNameLoc+45:whitworth.find('<', majorNameLoc+45)])
+
+    classNameLoc = whitworth.find('onclick="return showCourse(this,', startLoc, endLoc)
+    #find and print the first comment
+    commentLoc = whitworth.find('span class="courselistcomment"', startLoc, classNameLoc)
+
+    courseGroup = 0
+ 
+    #print("Locationnnnnn", commentLoc)
+    if commentLoc != -1:
+        comment = whitworth[commentLoc + 31:whitworth.find(" ", commentLoc+31)]
+        #print(comment)
+    else:
+        commentLoc = startLoc
+
+    #find the secind comment position
+    commentLoc = whitworth.find('span class="courselistcomment"', commentLoc+1, endLoc)
+
+
+    count = [0,0,0,0,0]
+    changeFlag = False
+
+    while classNameLoc != -1 :
+
+        if classNameLoc > commentLoc and commentLoc != -1:
+            changeFlag = True
+            comment = whitworth[commentLoc + 31:whitworth.find(" ", commentLoc + 31)]
+            #print(comment)
+            commentLoc = whitworth.find('span class="courselistcomment"', commentLoc+1, endLoc)
+
+        
+
+        #print(whitworth[classNameLoc+34:classNameLoc+40])#the full name
+        fullName = whitworth[classNameLoc+34:classNameLoc+40]#the full name
+        #print(fullName)
+        classNameLoc = whitworth.find('onclick="return showCourse(this,', classNameLoc+3, endLoc)#find the location for the next class name
+
+        #check if the department is in the list or not
+        #if not create one new, if there is, check if the class is in the list or not, if not add new, if already add to the repeated list
+        #print(fullName)
+        if (comment == "Core" and changeFlag == True):
+            count[0] += 1 
+            courseGroup = 0 
+            changeFlag = False
+        elif (comment == "One" and changeFlag == True): 
+            count[1] += 1
+            courseGroup = 10 + count[1]
+            changeFlag = False
+        elif (comment == "Two" and changeFlag == True): 
+            count[2] += 1
+            courseGroup = 20 + count[2]
+            changeFlag = False
+        elif (comment == "Three" and changeFlag == True):
+            count[3] += 1 
+            courseGroup = 30 + count[3]
+            changeFlag = False
+        elif (comment == "Four" and changeFlag == True): 
+            count[4] += 1
+            courseGroup = 40 + count [4]
+            changeFlag = False
+
+        listStore[courseGroup].append(fullName)
+
+PATH = "test4.html"
+path2 = "test41.html"
+r = requests.get("http://catalog.whitworth.edu/undergraduate/mathcomputerscience/#majors_optionstext")
+#write the html file
+#this file is currently used for debugging only and have no impact to the application
+f = open(PATH, "w")
+f.write(r.text)
+
+l = open(path2, "a")
+
+whitworth = r.text
+i = 0
+for line in whitworth.split("\n"):
+    if '"sc_courselist"' in line and i == 0:
+        print(line)
+        l.write(line+ "\n")
+
+
--- testTwo.py
+++ testTwo.py
@@ -1,14 +1,87 @@
+#https://stackoverflow.com/questions/960733/python-creating-a-dictionary-of-lists
+
 import requests
 import glob, os
 from collections import defaultdict
 
+#check if the department is in the list or not
+#if not create one new, if there is, check if the class is in the list or not, if not add new, if already add to the repeated list
+def addClass(majorName, fullName, className):
+    if fullName[:2] in className[majorName]:
+        if int(fullName[3:]) in className[majorName][fullName[:2]]:
+            #print("Repeated! ",fullName)
+            repeatedClass.append(fullName)
+        else:
+            className[majorName][fullName[:2]].append(int(fullName[3:]))
+    else:
+        try:
+            className[majorName][fullName[:2]].append(int(fullName[3:]))
+        except:
+            print("fail")
+
+
+
 PATH = "test2.html"
 r = requests.get("http://catalog.whitworth.edu/undergraduate/mathcomputerscience/#majors_optionstext")
 
-
+#write the html file
+#this file is currently used for debugging only and have no impact to the application
 f = open(PATH, "w")
 f.write(r.text)
 
-print(r.text)
+#print(r.text)#print to the console for debugging
+
+#whitworth string stores the whole html file
+whitworth = r.text
+
+#should be thee start location of the major
+startLoc = whitworth.find('"sc_courselist"')
+#end location of the same major in the same major
+endLoc = whitworth.find('"sc_courselist"', startLoc+1)
+
+#location for the class name
+classNameLoc = whitworth.find('onclick="return showCourse(this,', startLoc, endLoc)
+#location for the major name
+majorNameLoc = whitworth.find('"majors_optionstext">Requirements for a', startLoc, endLoc)
+
+className = defaultdict(lambda: defaultdict(list))
+
+repeatedClass = []
+majorName = ""
 
 
+while startLoc != -1:
+    classNameLoc = whitworth.find('onclick="return showCourse(this,', startLoc, endLoc)
+    #location for the major name
+    majorNameLoc = whitworth.find('name="majors_optionstext">Requirements for a', startLoc, endLoc)
+
+    #print (startLoc," Before")
+    #print the name of the major, end when find the '<'
+    if ( majorNameLoc != -1):
+        print(whitworth[majorNameLoc+45:whitworth.find('<', majorNameLoc+45)])
+        majorName = whitworth[majorNameLoc+45:whitworth.find('<', majorNameLoc+45)]
+        while classNameLoc != -1 :
+
+            #print(whitworth[classNameLoc+34:classNameLoc+40])#the full name
+            fullName = whitworth[classNameLoc+34:classNameLoc+40]#the full name
+            classNameLoc = whitworth.find('onclick="return showCourse(this,', classNameLoc+3, endLoc)#find the location for the next class name
+
+            #check if the department is in the list or not
+            #if not create one new, if there is, check if the class is in the list or not, if not add new, if already add to the repeated list
+            addClass(majorName, fullName, className)
+
+            
+
+        #print (startLoc)
+    startLoc = whitworth.find('"sc_courselist"', endLoc)
+    #end location of the same major in the same major
+    endLoc = whitworth.find('"sc_courselist"', startLoc+1)
+
+print(className)
+for x in className:
+    print(x)
+    for y in className[x]:
+        #print(y)
+        for z in className[x][y]:
+            print(y,z)
+#print(repeatedClass)
